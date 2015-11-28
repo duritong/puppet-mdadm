757740f0f378ab94d3492cba24dc868faea33d62
mh <mh@immerda.ch>
2015-11-28 17:41:36 +0100

    linting for future parser

--- manifests/mismatch_cnt.pp
+++ manifests/mismatch_cnt.pp
@@ -1,8 +1,11 @@
+# fix mismatch coung
 class mdadm::mismatch_cnt {
   require rubygems::chronic_duration
 
   file{'/usr/local/sbin/fix_md_mismatch_cnt':
     source => 'puppet:///modules/mdadm/scripts/fix_md_mismatch_cnt',
-    owner => root, group => 0, mode => 0700;
+    owner  => root,
+    group  => 0,
+    mode   => '0700';
   }
 }
