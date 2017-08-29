38746a06ed65826ef795e8cfb595315724b306df
mh <mh@immerda.ch>
2017-08-29 21:40:12 +0200

    switch to package

--- manifests/mismatch_cnt.pp
+++ manifests/mismatch_cnt.pp
@@ -1,6 +1,6 @@
 # fix mismatch coung
 class mdadm::mismatch_cnt {
-  require rubygems::chronic_duration
+  ensure_packages(['rubygem-chronic_duration'])
 
   file{'/usr/local/sbin/fix_md_mismatch_cnt':
     source => 'puppet:///modules/mdadm/scripts/fix_md_mismatch_cnt',
